538e4597ce56449f8d3bb84e84f0c3dc5e5713e1
AbhayBhan <abhaybhan197@gmail.com>
2022-08-08 09:09:54 +0530

    This is for checking purposes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,9 @@ export default function Navbar(props) {
           <li className="nav-item">
             <a className="nav-link" href="/">About</a>
           </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Help</a>
+          </li>
         </ul>
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search Notes" aria-label="Search"/>
